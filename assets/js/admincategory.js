25c0d99906567cec06f6363aef3ec70bdab5499d
Joaquin <joacovillamediana@gmail.com>
2021-11-12 01:39:14 -0300

    Ajustes

---
+++ assets/js/admincategory.js
@@ -0,0 +1,19 @@
+function validateFields() {
+    event.preventDefault();
+    var name = document.getElementById("name-input").value;
+
+    var errors = false;
+
+    if (name.length >= 3 && name.length < 60) {
+        document.getElementById("name-input").style.cssText = 'border: 1px solid #ced4da;';
+        document.querySelector("#name .error").style.cssText = 'display: none;';
+    } else {
+        document.getElementById("name-input").style.cssText = 'border-color: red';
+        document.querySelector("#name .error").style.cssText = 'display: block;';
+        errors = true;
+    }
+
+    if (!errors) {
+        window.location.href = "./index.html";
+    }
+}
